90d1a717661ece0e3408e05a9a962db6de97ed6d
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2021-12-26 21:26:11 -0600

    v3.0.2

--- notcurses/direct.nim
+++ notcurses/direct.nim
@@ -4,4 +4,4 @@ const notcursesAltNames {.strdefine.} = "libnotcurses"
 
 include ./includes/defines_notcurses_direct
 include ./includes/generator_top
-include ./includesg/generator_bottom_notcurses_direct
+include ./includes/generator_bottom_notcurses_direct
--- notcurses/includes/generator_top.nim
+++ notcurses/includes/generator_top.nim
@@ -16,7 +16,7 @@ const
 
   notcursesRepo {.strdefine.} = "https://github.com/dankamongmen/notcurses"
 
-  notcursesTag {.strdefine.} = "v3.0.1"
+  notcursesTag {.strdefine.} = "v3.0.2"
 
   notcursesDlUrl {.strdefine.} =
     fmt"{notcursesRepo}/archive/refs/tags/{notcursesTag}.tar.gz"
