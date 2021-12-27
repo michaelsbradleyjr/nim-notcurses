6a493f4bd7026e73f66feca15191e7bd1abd0614
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2021-12-26 23:24:24 -0600

    [wip] don't need notcurses/common.nim at present

--- notcurses/common.nim
+++
@@ -1,4 +0,0 @@
-# could import wrapper and provide shorter names for some constants,
-# e.g. NCOPTION_NO_ALTERNATE_SCREEN -> NoAlternateScreen
-
-# would need to import/export common from e.g. notcurses.nim and notcurses/core.nim
