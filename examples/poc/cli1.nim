1da43d3921013504140f54d878492337c65f3bd1
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2022-01-01 17:17:01 -0600

    [wip] nimlsp started honoring unprefixed import of notcurses
    I'm not sure what caused the change, but it's working now so get rid of the
    unnecessary comments and alternative imports using relative paths

--- examples/poc/cli1.nim
+++ examples/poc/cli1.nim
@@ -1,11 +1,4 @@
-# import notcurses/cli
-
-# The Nim compiler can rely on ../pkg.cfg (--path setting) to load unprefixed
-# notcurses. However, some tools, e.g. nimlsp in combo with Emacs lsp-mode, can
-# (currently) fail to handle nim.cfg correctly, in which case missing symbols,
-# etc. may be reported in one's editor; it's preferable to import relative
-# paths while developing with tools suffering such limitations.
-import ../../notcurses/cli
+import notcurses/cli
 
 let
   nc = Nc.init
