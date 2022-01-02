3393b8145c28991a753e587b3dc05c837dd6be3b
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2022-01-01 18:59:56 -0600

    [wip] remove a remaining comment re: importing with relative paths

--- examples/poc/cli3.nim
+++ examples/poc/cli3.nim
@@ -1,12 +1,5 @@
 import notcurses
 
-# The Nim compiler can rely on ../pkg.cfg (--path setting) to load unprefixed
-# notcurses. However, some tools, e.g. nimlsp in combo with Emacs lsp-mode, can
-# (currently) fail to handle nim.cfg correctly, in which case missing symbols,
-# etc. may be reported in one's editor; it's preferable to import relative
-# paths while developing with tools suffering such limitations.
-# import ../../notcurses
-
 let
   opts = [DrainInput, NoClearBitmaps, PreserveCursor]
   nc = Nc.init NcOpts.init opts
