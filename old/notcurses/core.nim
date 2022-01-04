9664250276b15704a44c0f0f10db56441e3a5e21
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2022-01-03 23:24:54 -0600

    [wip] rm -rf old, not needed on this branch

--- old/notcurses/core.nim
+++
@@ -1,7 +0,0 @@
-include ./includes/imports
-
-const notcursesAltNames {.strdefine.} = "libnotcurses-core.3"
-
-include ./includes/defines_notcurses
-include ./includes/generator_top
-include ./includes/generator_bottom_notcurses
--- old/notcurses/direct.nim
+++
@@ -1,7 +0,0 @@
-include ./includes/imports
-
-const notcursesAltNames {.strdefine.} = "libnotcurses.3"
-
-include ./includes/defines_notcurses_direct
-include ./includes/generator_top
-include ./includes/generator_bottom_notcurses_direct
