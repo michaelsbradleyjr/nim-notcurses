5f159d0afc7b5526726e78eb526b7536a8afe7ad
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2022-01-17 16:25:59 -0600

    [wip] refactor abi/private re: libnotcurses-ffi and notcurses_init_lib

--- notcurses/abi/private.nim
+++ notcurses/abi/private.nim
@@ -1,68 +1,71 @@
 type NotcursesDefect = object of Defect
 
 const
-  notcurses_header = "notcurses/notcurses.h"
-  notcurses_init_name = notcurses_init_import_prefix & "init"
+  nc_header = "notcurses/notcurses.h"
+  nc_init_name = notcurses_init_prefix & "init"
 
-proc notcurses_version_components(major, minor, patch, tweak: ptr cint)
-  {.cdecl, dynlib: notcurses_lib, importc.}
+{.pragma: nc, cdecl, dynlib: notcurses_lib, importc.}
 
 var notcurses_major, notcurses_minor, notcurses_patch, notcurses_tweak: cint
+{.pragma: nc_bycopy, bycopy, header: nc_header.}
 
 notcurses_version_components(addr notcurses_major, addr notcurses_minor,
   addr notcurses_patch, addr notcurses_tweak)
+{.pragma: nc_incomplete, header: nc_header, incompleteStruct.}
 
 if nim_notcurses_version.major != notcurses_major:
+{.pragma: nc_init, cdecl, dynlib: notcurses_init_lib, importc: nc_init_name.}
+
+proc notcurses_version_components(major, minor, patch, tweak: ptr cint) {.nc.}
   raise (ref NotcursesDefect)(msg:
     "nim-notcurses major version " & $nim_notcurses_version.major & " " &
     "is not compatible with Notcurses library major version " &
     $notcurses_major)
 
 type
-  ncinput {.bycopy, header: notcurses_header,
-      importc: "struct ncinput".} = object
-    alt*, ctrl*, shift*: bool
-    evtype*: NCTYPES
+  ncinput {.nc_bycopy, importc: "struct ncinput".} = object
     id*: uint32
+    y*: cint
+    x*: cint
     utf8*: array[5, cchar]
-    x*, xpx*, y*, ypx*: cint
+    alt*: bool
+    shift*: bool
+    ctrl*: bool
+    evtype*: NCTYPE
+    modifiers*: cuint
+    ypx*: cint
+    xpx*: cint
 
-  ncplane {.header: notcurses_header, importc: "struct ncplane",
-    incompleteStruct.} = object
+  ncplane {.nc_incomplete, importc: "struct ncplane".} = object
 
-  notcurses {.header: notcurses_header, importc: "struct notcurses",
-    incompleteStruct.} = object
+  notcurses {.nc_incomplete, importc: "struct notcurses".} = object
 
-  notcurses_options {.bycopy, header: notcurses_header,
-      importc: "struct notcurses_options".} = object
+  notcurses_options {.nc_bycopy, importc: "struct notcurses_options".} = object
+    termtype*: cstring
+    loglevel*: ncloglevel_e
+    margin_t*: cuint
+    margin_r*: cuint
+    margin_b*: cuint
+    margin_l*: cuint
     flags*: culonglong
 
-proc ncpile_rasterize(n: ptr ncplane): cint
-  {.cdecl, dynlib: notcurses_lib, importc.}
+proc ncpile_rasterize(n: ptr ncplane): cint {.nc.}
 
-proc ncpile_render(n: ptr ncplane): cint
-  {.cdecl, dynlib: notcurses_lib, importc.}
+proc ncpile_render(n: ptr ncplane): cint {.nc.}
 
-proc ncplane_putstr(n: ptr ncplane, gclustarr: cstring): cint
-  {.cdecl, header: notcurses_header, importc.}
+proc ncplane_putstr(n: ptr ncplane, gclustarr: cstring): cint {.nc.}
 
-proc ncplane_set_scrolling(n: ptr ncplane, scrollp: cuint): bool
-  {.cdecl, dynlib: notcurses_lib, importc.}
+proc ncplane_set_scrolling(n: ptr ncplane, scrollp: cuint): bool {.nc.}
 
-proc notcurses_get_blocking(n: ptr notcurses; ni: ptr ncinput): uint32
-  {.cdecl, header: notcurses_header, importc.}
+proc notcurses_get_blocking(n: ptr notcurses; ni: ptr ncinput): uint32 {.nc.}
 
 proc notcurses_init(opts: ptr notcurses_options, fp: File): ptr notcurses
-  {.cdecl, dynlib: notcurses_lib, importc: notcurses_init_name.}
+  {.nc_init.}
 
-proc notcurses_render(nc: ptr notcurses): cint
-  {.cdecl, header: notcurses_header, importc.}
+proc notcurses_render(nc: ptr notcurses): cint {.nc.}
 
-proc notcurses_stdplane(nc: ptr notcurses): ptr ncplane
-  {.cdecl, dynlib: notcurses_lib, importc.}
+proc notcurses_stdplane(nc: ptr notcurses): ptr ncplane {.nc.}
 
-proc notcurses_stop(nc: ptr notcurses): cint
-  {.cdecl, dynlib: notcurses_lib, importc.}
+proc notcurses_stop(nc: ptr notcurses): cint {.nc.}
 
-proc notcurses_version(): cstring
-  {.cdecl, dynlib: notcurses_lib, importc.}
+proc notcurses_version(): cstring {.nc.}
