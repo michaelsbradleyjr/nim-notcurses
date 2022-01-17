98bcdf1ad5218e863da380f310bcfbeba7fb00e6
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2022-01-17 16:32:25 -0600

    [wip] rm some unnecessary commas

--- notcurses/api/private/constants.nim
+++ notcurses/api/private/constants.nim
@@ -15,14 +15,14 @@ type
   NotcursesInitOption = distinct culonglong
 
   NotcursesInitOptions {.pure.} = enum
-    InhibitSetlocale  = NCOPTION_INHIBIT_SETLOCALE.NotcursesInitOption,
-    NoClearBitmaps    = NCOPTION_NO_CLEAR_BITMAPS.NotcursesInitOption,
-    NoWinchSighandler = NCOPTION_NO_WINCH_SIGHANDLER.NotcursesInitOption,
-    NoQuitSighandlers = NCOPTION_NO_QUIT_SIGHANDLERS.NotcursesInitOption,
-    PreserveCursor    = NCOPTION_PRESERVE_CURSOR.NotcursesInitOption,
-    SuppressBanners   = NCOPTION_SUPPRESS_BANNERS.NotcursesInitOption,
-    NoAlternateScreen = NCOPTION_NO_ALTERNATE_SCREEN.NotcursesInitOption,
-    NoFontChanges     = NCOPTION_NO_FONT_CHANGES.NotcursesInitOption,
+    InhibitSetlocale  = NCOPTION_INHIBIT_SETLOCALE.NotcursesInitOption
+    NoClearBitmaps    = NCOPTION_NO_CLEAR_BITMAPS.NotcursesInitOption
+    NoWinchSighandler = NCOPTION_NO_WINCH_SIGHANDLER.NotcursesInitOption
+    NoQuitSighandlers = NCOPTION_NO_QUIT_SIGHANDLERS.NotcursesInitOption
+    PreserveCursor    = NCOPTION_PRESERVE_CURSOR.NotcursesInitOption
+    SuppressBanners   = NCOPTION_SUPPRESS_BANNERS.NotcursesInitOption
+    NoAlternateScreen = NCOPTION_NO_ALTERNATE_SCREEN.NotcursesInitOption
+    NoFontChanges     = NCOPTION_NO_FONT_CHANGES.NotcursesInitOption
     DrainInput        = NCOPTION_DRAIN_INPUT.NotcursesInitOption
 
   NotcursesInputEvent = distinct cint
