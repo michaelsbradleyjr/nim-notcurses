c43d8284491d7460b6760fba6d5c14d74803799a
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2022-01-17 16:20:56 -0600

    [wip] refactor public abi modules re: libnotcurses-ffi and notcurses_init_lib

--- notcurses/abi.nim
+++ notcurses/abi.nim
@@ -1,19 +1,21 @@
-when defined(macosx):
-  {.passL: "-lnotcurses.3 -lnotcurses-core.3".}
-elif defined(windows):
-  {.passL: "-lnotcurses -lnotcurses-core".}
-else:
-  {.passL: "-l:libnotcurses.so.3 -l:libnotcurses-core.so.3".}
-
 const
-  notcurses_init_import_prefix = "notcurses_"
-  notcurses_lib =
+  notcurses_init_lib =
     when defined(macosx):
       "libnotcurses.3.dylib"
     elif defined(windows):
       "libnotcurses.dll"
     else:
       "libnotcurses.so.3"
+
+  notcurses_init_prefix = "notcurses_"
+
+  notcurses_lib =
+    when defined(macosx):
+      "libnotcurses-ffi.3.dylib"
+    elif defined(windows):
+      "libnotcurses-ffi.dll"
+    else:
+      "libnotcurses-ffi.so.3"
 
 include ./abi/private/common
 include ./abi/private/constants
--- notcurses/abi/core.nim
+++ notcurses/abi/core.nim
@@ -1,19 +1,21 @@
-when defined(macosx):
-  {.passL: "-lnotcurses-core.3".}
-elif defined(windows):
-  {.passL: "-lnotcurses-core".}
-else:
-  {.passL: "-l:libnotcurses-core.so.3".}
-
 const
-  notcurses_init_import_prefix = "notcurses_core_"
-  notcurses_lib =
+  notcurses_init_lib =
     when defined(macosx):
       "libnotcurses-core.3.dylib"
     elif defined(windows):
       "libnotcurses-core.dll"
     else:
       "libnotcurses-core.so.3"
+
+  notcurses_init_prefix = "notcurses_core_"
+
+  notcurses_lib =
+    when defined(macosx):
+      "libnotcurses-ffi.3.dylib"
+    elif defined(windows):
+      "libnotcurses-ffi.dll"
+    else:
+      "libnotcurses-ffi.so.3"
 
 include ./private/common
 include ./private/constants
