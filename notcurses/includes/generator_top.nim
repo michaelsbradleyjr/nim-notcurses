8d0b6b6e934a711e2b14d98502a88f4ff725a1cb
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2022-12-25 01:10:33 -0600

    v3.0.8

--- notcurses/includes/generator_top.nim
+++ notcurses/includes/generator_top.nim
@@ -16,7 +16,7 @@ const
 
   notcursesRepo {.strdefine.} = "https://github.com/dankamongmen/notcurses"
 
-  notcursesTag {.strdefine.} = "v3.0.7"
+  notcursesTag {.strdefine.} = "v3.0.8"
 
   notcursesDlUrl {.strdefine.} =
     fmt"{notcursesRepo}/archive/refs/tags/{notcursesTag}.tar.gz"
