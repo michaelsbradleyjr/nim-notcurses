b7a9a56276a73e082ea035f887d1bfe83a9cec29
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2023-01-25 16:33:04 -0600

    [wip] call putStr in cli1 example, and add some comments

--- examples/poc/cli1.nim
+++ examples/poc/cli1.nim
@@ -3,4 +3,12 @@ import notcurses/cli
 
 let nc = Nc.init NcOptions.init DrainInput
 
-nc.render.expect
+# there are one/more bugs in Notcurses affecting its CLI mode that result in
+# incorrect scrolling/rendering, especially noticeable when init option
+# SuppressBanners is not used
+#
+# possibly related to https://github.com/dankamongmen/notcurses/issues/2196
+
+# in Notcurses' CLI mode \n in a string triggers a render, one or more times
+# depending on the number of times it's included
+nc.stdPlane.putStr("Hello, Notcurses!\n").expect
