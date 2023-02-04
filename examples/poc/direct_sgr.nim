b18f2da857d5d7dc5f1bd3b021558e3c4fd55d83
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2023-02-04 14:17:05 -0600

    [wip] rev direct_sgr example to use api layer

--- examples/poc/direct_sgr.nim
+++ examples/poc/direct_sgr.nim
@@ -1,6 +1,17 @@
+import std/strutils
 import notcurses/direct/core
 # or: import notcurses/direct
 
 let ncd = Ncd.init NcdOptions.init [DirectDrainInput]
 
-ncd.putStr("Do SGR stuff with Direct mode\n").expect
+# https://en.wikipedia.org/wiki/ANSI_escape_code#SGR
+var e, i = 0'u32
+while true:
+  if bitand(ncd.supportedStyles.uint32, i) == i:
+    ncd.setStyles(cast[NcStyles](i)).expect
+  ncd.putStr(i.toHex.toLower & " ").expect
+  ncd.setStyles(NcStyles.None).expect
+  inc e
+  if e mod 8 == 0: ncd.putStr("\n").expect
+  inc i
+  if i >= (NcStyles.Italic.uint32 shl 1): break
