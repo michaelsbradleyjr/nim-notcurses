5593a6e8e4b01b677ae8f5596299d958e138cb75
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2023-03-17 11:25:37 -0500

    [wip] use importc for wide strings declared in ncseqs.h

--- notcurses/abi/constants.nim
+++ notcurses/abi/constants.nim
@@ -415,94 +415,77 @@ const
   NCDIRECT_OPTION_VERBOSE*             = 0x0000000000000010'u64
   NCDIRECT_OPTION_VERY_VERBOSE*        = 0x0000000000000020'u64
 
-# use importc for these constants so values are a match for Notcurses' raw C
-# API, which declares them as wide string literals
+const nc_seqs_header = "notcurses/ncseqs.h"
+{.pragma: nc_seqs, header: nc_seqs_header, importc, nodecl.}
 
-const
+let
   # L9 - notcurses/ncseqs.h
-  NCBOXLIGHTW*  = "┌┐└┘─│".cstring
-  NCBOXHEAVYW*  = "┏┓┗┛━┃".cstring
-  NCBOXROUNDW*  = "╭╮╰╯─│".cstring
-  NCBOXDOUBLEW* = "╔╗╚╝═║".cstring
-  NCBOXASCIIW*  = "/\\\\/-|".cstring
-  NCBOXOUTERW*  = "🭽🭾🭼🭿▁🭵🭶🭰".cstring
+  NCBOXLIGHTW*  {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCBOXHEAVYW*  {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCBOXROUNDW*  {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCBOXDOUBLEW* {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCBOXASCIIW*  {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCBOXOUTERW*  {.nc_seqs.}: ptr UncheckedArray[wchar_t]
 
   # L17 - notcurses/ncseqs.h
-  NCWHITESQUARESW*   = "◲◱◳◰".cstring
-  NCWHITECIRCLESW*   = "◶◵◷◴".cstring
-  NCCIRCULARARCSW*   = "◜◝◟◞".cstring
-  NCWHITETRIANGLESW* = "◿◺◹◸".cstring
-  NCBLACKTRIANGLESW* = "◢◣◥◤".cstring
-  NCSHADETRIANGLESW* = "🮞🮟🮝🮜".cstring
+  NCWHITESQUARESW*   {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCWHITECIRCLESW*   {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCCIRCULARARCSW*   {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCWHITETRIANGLESW* {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCBLACKTRIANGLESW* {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCSHADETRIANGLESW* {.nc_seqs.}: ptr UncheckedArray[wchar_t]
 
   # L25 - notcurses/ncseqs.h
-  NCBLACKARROWHEADSW* = "⮝⮟⮜⮞".cstring
-  NCLIGHTARROWHEADSW* = "⮙⮛⮘⮚".cstring
-  NCARROWDOUBLEW*     = "⮅⮇⮄⮆".cstring
-  NCARROWDASHEDW*     = "⭫⭭⭪⭬".cstring
-  NCARROWCIRCLEDW*    = "⮉⮋⮈⮊".cstring
-  NCARROWANTICLOCKW*  = "⮏⮍⮎⮌".cstring
-  NCBOXDRAWW*         = "╵╷╴╶".cstring
-  NCBOXDRAWHEAVYW*    = "╹╻╸╺".cstring
+  NCBLACKARROWHEADSW* {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCLIGHTARROWHEADSW* {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCARROWDOUBLEW*     {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCARROWDASHEDW*     {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCARROWCIRCLEDW*    {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCARROWANTICLOCKW*  {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCBOXDRAWW*         {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCBOXDRAWHEAVYW*    {.nc_seqs.}: ptr UncheckedArray[wchar_t]
 
   # L35 - notcurses/ncseqs.h
-  NCARROWW*     = "⭡⭣⭠⭢⭧⭩⭦⭨".cstring
-  NCDIAGONALSW* = "🮣🮠🮡🮢🮤🮥🮦🮧".cstring
+  NCARROWW*     {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCDIAGONALSW* {.nc_seqs.}: ptr UncheckedArray[wchar_t]
 
   # L39 - notcurses/ncseqs.h
-  NCDIGITSSUPERW* = "⁰¹²³⁴⁵⁶⁷⁸⁹".cstring
-  NCDIGITSSUBW*   = "₀₁₂₃₄₅₆₇₈₉".cstring
+  NCDIGITSSUPERW* {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCDIGITSSUBW*   {.nc_seqs.}: ptr UncheckedArray[wchar_t]
 
   # L43 - notcurses/ncseqs.h
-  NCASTERISKS5* = "🞯🞰🞱🞲🞳🞴".cstring
-  NCASTERISKS6* = "🞵🞶🞷🞸🞹🞺".cstring
-  NCASTERISKS8* = "🞻🞼✳🞽🞾🞿".cstring
+  NCASTERISKS5* {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCASTERISKS6* {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCASTERISKS8* {.nc_seqs.}: ptr UncheckedArray[wchar_t]
 
   # L48 - notcurses/ncseqs.h
-  NCANGLESBR* = "🭁🭂🭃🭄🭅🭆🭇🭈🭉🭊🭋".cstring
-  NCANGLESTR* = "🭒🭓🭔🭕🭖🭧🭢🭣🭤🭥🭦".cstring
-  NCANGLESBL* = "🭌🭍🭎🭏🭐🭑🬼🬽🬾🬿🭀".cstring
-  NCANGLESTL* = "🭝🭞🭟🭠🭡🭜🭗🭘🭙🭚🭛".cstring
-  NCEIGHTHSB* = " ▁▂▃▄▅▆▇█".cstring
-  NCEIGHTHST* = " ▔🮂🮃▀🮄🮅🮆█".cstring
-  NCEIGHTHSL* = "▏▎▍▌▋▊▉█".cstring
-  NCEIGHTHSR* = "▕🮇🮈▐🮉🮊🮋█".cstring
-  NCHALFBLOCKS* = " ▀▄█".cstring
-  NCQUADBLOCKS* = " ▘▝▀▖▌▞▛▗▚▐▜▄▙▟█".cstring
-  NCSEXBLOCKS*  = " 🬀🬁🬂🬃🬄🬅🬆🬇🬈🬊🬋🬌🬍🬎🬏🬐🬑🬒🬓▌🬔🬕🬖🬗🬘🬙🬚🬛🬜🬝🬞🬟🬠🬡🬢🬣🬤🬥🬦🬧▐🬨🬩🬪🬫🬬🬭🬮🬯🬰🬱🬲🬳🬴🬵🬶🬷🬸🬹🬺🬻█".cstring
+  NCANGLESBR*   {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCANGLESTR*   {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCANGLESBL*   {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCANGLESTL*   {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCEIGHTHSB*   {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCEIGHTHST*   {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCEIGHTHSL*   {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCEIGHTHSR*   {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCHALFBLOCKS* {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCQUADBLOCKS* {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCSEXBLOCKS*  {.nc_seqs.}: ptr UncheckedArray[wchar_t]
 
   # L59 - notcurses/ncseqs.h
-  NCBRAILLEEGCS* =
-    ("\u2800\u2801\u2808\u2809\u2802\u2803\u280a\u280b\u2810\u2811\u2818\u2819\u2812\u2813\u281a\u281b" &
-     "\u2804\u2805\u280c\u280d\u2806\u2807\u280e\u280f\u2814\u2815\u281c\u281d\u2816\u2817\u281e\u281f" &
-     "\u2820\u2821\u2828\u2829\u2822\u2823\u282a\u282b\u2830\u2831\u2838\u2839\u2832\u2833\u283a\u283b" &
-     "\u2824\u2825\u282c\u282d\u2826\u2827\u282e\u282f\u2834\u2835\u283c\u283d\u2836\u2837\u283e\u283f" &
-     "\u2840\u2841\u2848\u2849\u2842\u2843\u284a\u284b\u2850\u2851\u2858\u2859\u2852\u2853\u285a\u285b" &
-     "\u2844\u2845\u284c\u284d\u2846\u2847\u284e\u284f\u2854\u2855\u285c\u285d\u2856\u2857\u285e\u285f" &
-     "\u2860\u2861\u2868\u2869\u2862\u2863\u286a\u286b\u2870\u2871\u2878\u2879\u2872\u2873\u287a\u287b" &
-     "\u2864\u2865\u286c\u286d\u2866\u2867\u286e\u286f\u2874\u2875\u287c\u287d\u2876\u2877\u287e\u287f" &
-     "\u2880\u2881\u2888\u2889\u2882\u2883\u288a\u288b\u2890\u2891\u2898\u2899\u2892\u2893\u289a\u289b" &
-     "\u2884\u2885\u288c\u288d\u2886\u2887\u288e\u288f\u2894\u2895\u289c\u289d\u2896\u2897\u289e\u289f" &
-     "\u28a0\u28a1\u28a8\u28a9\u28a2\u28a3\u28aa\u28ab\u28b0\u28b1\u28b8\u28b9\u28b2\u28b3\u28ba\u28bb" &
-     "\u28a4\u28a5\u28ac\u28ad\u28a6\u28a7\u28ae\u28af\u28b4\u28b5\u28bc\u28bd\u28b6\u28b7\u28be\u28bf" &
-     "\u28c0\u28c1\u28c8\u28c9\u28c2\u28c3\u28ca\u28cb\u28d0\u28d1\u28d8\u28d9\u28d2\u28d3\u28da\u28db" &
-     "\u28c4\u28c5\u28cc\u28cd\u28c6\u28c7\u28ce\u28cf\u28d4\u28d5\u28dc\u28dd\u28d6\u28d7\u28de\u28df" &
-     "\u28e0\u28e1\u28e8\u28e9\u28e2\u28e3\u28ea\u28eb\u28f0\u28f1\u28f8\u28f9\u28f2\u28f3\u28fa\u28fb" &
-     "\u28e4\u28e5\u28ec\u28ed\u28e6\u28e7\u28ee\u28ef\u28f4\u28f5\u28fc\u28fd\u28f6\u28f7\u28fe\u28ff").cstring
+  NCBRAILLEEGCS* {.nc_seqs.}: ptr UncheckedArray[wchar_t]
 
   # L76 - notcurses/ncseqs.h
-  NCSEGDIGITS* =
-    ("\u{0001fbf0}\u{0001fbf1}\u{0001fbf2}\u{0001fbf3}\u{0001fbf4}" &
-     "\u{0001fbf5}\u{0001fbf6}\u{0001fbf7}\u{0001fbf8}\u{0001fbf9}").cstring
+  NCSEGDIGITS* {.nc_seqs.}: ptr UncheckedArray[wchar_t]
 
   # L79 - notcurses/ncseqs.h
-  NCSUITSBLACK* = "\u2660\u2663\u2665\u2666".cstring # ♠♣♥♦
-  NCSUITSWHITE* = "\u2661\u2662\u2664\u2667".cstring # ♡♢♤♧
-  NCCHESSBLACK* = "\u265f\u265c\u265e\u265d\u265b\u265a".cstring # ♟♜♞♝♛♚
-  NCCHESSWHITE* = "\u265f\u265c\u265e\u265d\u265b\u265a".cstring # ♙♖♘♗♕♔
-  NCDICE*       = "\u2680\u2681\u2682\u2683\u2684\u2685".cstring # ⚀⚁⚂⚃⚄⚅
-  NCMUSICSYM*   = "\u2669\u266A\u266B\u266C\u266D\u266E\u266F".cstring # ♩♪♫♬♭♮♯
+  NCSUITSBLACK* {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCSUITSWHITE* {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCCHESSBLACK* {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCCHESSWHITE* {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCDICE*       {.nc_seqs.}: ptr UncheckedArray[wchar_t]
+  NCMUSICSYM*   {.nc_seqs.}: ptr UncheckedArray[wchar_t]
 
+const
   # L87 - notcurses/ncseqs.h
   NCBOXLIGHT*  = "┌┐└┘─│".cstring
   NCBOXHEAVY*  = "┏┓┗┛━┃".cstring
