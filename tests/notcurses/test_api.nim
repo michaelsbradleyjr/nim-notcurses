0cf1582a9e0ac51a358ce02cb0ce435f00cadbcc
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2023-03-27 21:02:19 -0500

    refactor: define wide strings from notcurses/ncseqs.h as Nim const

--- tests/notcurses/test_api.nim
+++ tests/notcurses/test_api.nim
@@ -1,17 +1,22 @@
 import pkg/unittest2
 import notcurses
 
-suite "API tests":
-  setup:
-    let
-      opts = [CliMode, DrainInput, SuppressBanners]
-      nc = Nc.init(NcOptions.init opts, addExitProc = false)
+when not defined(windows):
+  suite "API tests":
+    setup:
+      let
+        opts = [CliMode, DrainInput, SuppressBanners]
+        nc = Nc.init(NcOptions.init opts, addExitProc = false)
 
-  teardown:
-    nc.stop
+    teardown:
+      nc.stop
 
-  test "test 1":
-    check: true
+    test "test 1":
+      check: true
 
-  test "test 2":
+    test "test 2":
+      check: true
+
+suite "API tests (no init)":
+  test "test 3":
     check: true
