990ec8cb2c44923559b3fc4825f519a056fcf0e9
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2023-04-20 12:00:53 -0500

    chore: provide better support for nimlangserver per vscode and emacs lsp-mode

---
+++ extras/lsp.nim
@@ -0,0 +1,28 @@
+{.warning: "This module is not intended for compilation!".}
+
+# this is a support module for nimlangserver so that a single nimsuggest
+# process can service all modules in nim-notcurses; see ./project-mapping.el
+# and ../.vscode/settings.json
+
+import
+  examples/cjkscroll,
+  examples/cli1,
+  examples/cli2,
+  examples/direct1,
+  examples/direct_sgr,
+  examples/gradients,
+  examples/multiselect,
+  examples/tui1,
+  examples/zalgo,
+  notcurses,
+  notcurses/abi,
+  notcurses/abi/core,
+  notcurses/abi/direct,
+  notcurses/abi/direct/core as abi_ncd_core,
+  notcurses/core as nc_core,
+  notcurses/direct as ncd,
+  notcurses/direct/core as ncd_core,
+  notcurses/locale,
+  tests/test_all
+
+{.warning[UnusedImport]: off.}
