49fb082a1f340d0ad0fda98314072a45d68b53d1
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2023-04-26 20:24:34 -0500

    refactor: change how init proc is setup in entry point modules
    it needs to be possible to `import notcurses` and `import notcurses/core` in
    the same codebase (same is true for `import notcurses/direct` and
    `import notcurses/direct/core`)
    
    for example: a library author might build on `notcurses/core`, while a user of
    that library may wish to use it and the multimedia facilities made available
    via `import notcurses`

--- notcurses.nim
+++ notcurses.nim
@@ -1,7 +1,15 @@
 {.passL: "-lnotcurses -lnotcurses-core".}
 
+when (NimMajor, NimMinor, NimPatch) >= (1, 4, 0):
+  {.push raises: [].}
+else:
+  {.push raises: [Defect].}
+
+import ./notcurses/abi/impl as abi_impl
 import ./notcurses/api/impl
 
-export impl except setNcInit, setNcCoreInit
+export impl
 
-setNcInit()
+proc init*(T: type Notcurses, options = Options.init, file = stdout,
+    addExitProc = true): T =
+  T.init(notcurses_init, options, file, addExitProc)
--- notcurses/core.nim
+++ notcurses/core.nim
@@ -1,7 +1,15 @@
 {.passL: "-lnotcurses-core".}
 
+when (NimMajor, NimMinor, NimPatch) >= (1, 4, 0):
+  {.push raises: [].}
+else:
+  {.push raises: [Defect].}
+
+import ./abi/impl as abi_impl
 import ./api/impl
 
-export impl except setNcInit, setNcCoreInit
+export impl
 
-setNcCoreInit()
+proc init*(T: type Notcurses, options = Options.init, file = stdout,
+    addExitProc = true): T =
+  T.init(notcurses_core_init, options, file, addExitProc)
