48628f8275d243ad7350c1a2765ffc37ac6cf249
Michael Bradley <michaelsbradleyjr@gmail.com>
2023-05-09 09:43:50 -0500

    refactor: use std/sets for supportedStyles and supportedStylesMask in api/direct/impl (#23)
    * refactor: use std/sets for supportedStyles and supportedStylesMask in api/direct/impl
    
    * chore: use refactored supportedStyles in examples/direct_sgr

--- examples/direct_sgr.nim
+++ examples/direct_sgr.nim
@@ -1,18 +1,17 @@
-import std/strutils
+import std/[sequtils, strutils]
 import pkg/notcurses/direct/core
 # or: import pkg/notcurses/direct
 
 # https://en.wikipedia.org/wiki/ANSI_escape_code#SGR
 
-let ncd = Ncd.init NcdOpts.init [InitFlags.DrainInput]
-
-func style(i: uint32): Styles = cast[Styles](i)
-
-proc supported(i: uint32): bool = (ncd.supportedStyles.uint32 and i) == i
+let
+  ncd = Ncd.init NcdOpts.init [InitFlags.DrainInput]
+  supportedStyles = ncd.supportedStyles
 
 var e, i = 0'u32
 while i < (Styles.Italic.uint32 shl 1):
-  if i.supported: ncd.setStyles(i.style).expect
+  let styles = ncd.supportedStyles(Style(i)).intersection supportedStyles
+  ncd.setStyles(styles.toSeq).expect
   ncd.putStr(i.uint64.toHex(8).toLower & " ").expect
   ncd.setStyles(Styles.None).expect
   inc e
