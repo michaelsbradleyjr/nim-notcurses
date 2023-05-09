339e62093e456d8995544ec75a3e55be7bf98b31
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2023-05-09 12:28:28 -0500

    chore: cleanup tests, consistent naming, etc.

--- tests/notcurses/test_abi.nim
+++ tests/notcurses/test_abi.nim
@@ -40,8 +40,7 @@ when not defined(windows):
 # wide string literals in notcurses/notcurses.h are defined in abi/wide.nim as
 # Nim `const` via `macro L` that calculates at compile-time arrays of wchar_t
 # (type Wchar); it's also possible to access those wide strings via importc
-# (two possibilities: as normal arrays of known length, or as ptr
-# UncheckedArray)
+# (two possibilities: as normal array of known length, or as ptr UncheckedArray)
 
 # to ensure that `macro L` is implemented correctly, a test is provided below
 # that compares arrays generated with `macro L` and those accessed via importc
@@ -49,45 +48,46 @@ when not defined(windows):
 macro mkImpc(names: static openArray[string]): untyped =
   # debugEcho names
   result = newStmtList()
+  result.add quote do:
+    {.pragma: nc_seqs, header: "notcurses/ncseqs.h", nodecl.}
   for name in names:
     let
-      cname = ident name
+      const_id = ident name
       impc_aw = ident(name & "_impc_aw")
       impc_puaw = ident(name & "_impc_puaw")
     result.add quote do:
-      {.pragma: nc_seqs, header: "notcurses/ncseqs.h", nodecl.}
       # can be `let` instead of `var` with recent enough releases of Nim 1.4+
       var
-        `impc_aw` {.nc_seqs, importc: `name`.}: array[`cname`.len, Wchar]
+        `impc_aw` {.nc_seqs, importc: `name`.}: array[`const_id`.len, Wchar]
         `impc_puaw` {.nc_seqs, importc: `name`.}: ptr UncheckedArray[Wchar]
   # debugEcho toStrLit(result)
 
-mkImpc ncWideSeqsNames
+mkImpc ncWideStringsNames
 
 macro compareW(names: static openArray[string]): untyped =
   # debugEcho names
   result = newStmtList()
   for name in names:
     let
-      wa = ident name
-      wa_impc_aw = ident(name & "_impc_aw")
-      wa_impc_puaw = ident(name & "_impc_puaw")
+      const_id = ident name
+      impc_aw = ident(name & "_impc_aw")
+      impc_puaw = ident(name & "_impc_puaw")
     result.add quote do:
       var i = 0
       while true:
-        let wc = `wa_impc_puaw`[][i]
+        let wc = `impc_puaw`[][i]
         if wc == 0.wchar: break
         inc i
-      check: i + 1 == `wa`.len
-      for j in 0..<`wa`.len:
+      check: i + 1 == `const_id`.len
+      for j in 0..<`const_id`.len:
         check:
-          `wa_impc_aw`[j] == `wa`[j]
-          `wa_impc_puaw`[][j] == `wa`[j]
+          `impc_aw`[j] == `const_id`[j]
+          `impc_puaw`[][j] == `const_id`[j]
   # debugEcho toStrLit(result)
 
 suite "ABI (no init)":
   test "compare importc'd wide strings with constants":
-    compareW ncWideSeqsNames
+    compareW ncWideStringsNames
 
 suite "ABI Direct mode (no init)":
   test "should write some tests":
