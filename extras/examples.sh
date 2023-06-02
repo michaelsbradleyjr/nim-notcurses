e049a95ea71681d70f425eadb6d176c45ffe7a4f
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2023-06-02 14:19:47 -0500

    refactor: move array of example modules into a separate extras script

---
+++ extras/examples.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+declare -a modules=(
+  abi/cjkscroll
+  abi/cli1
+  abi/cli2
+  abi/direct1
+  abi/direct_sgr
+  abi/gradients
+  abi/multiselect
+  abi/tui1
+  abi/zalgo
+  cjkscroll
+  cli1
+  cli2
+  direct1
+  direct_sgr
+  gradients
+  tui1
+  zalgo
+)
